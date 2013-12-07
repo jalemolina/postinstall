0bf8ddb745c93d98b216affb3603f1ce5f143a6a
Alejandro Molina <jalemolina@gmail.com>
2013-12-07 18:15:53 -0300

    peleando todavía

--- Inconsolata-Powerline/Inconsolata-Powerline.sh
+++ Inconsolata-Powerline/Inconsolata-Powerline.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+# Ejecutar como admin.
+
 
 cp -r etc/ /etc/
 cp -r usr/ /usr/
+
+cp 10-powerline-symbols.conf ~/.fonts.conf.d/
+cp 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+cp PowerlineSymbols.otf ~/.fonts/
+
+fc-cache -vf ~/.fonts
+fc-cache -vf /usr/share/fonts
