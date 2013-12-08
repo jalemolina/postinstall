f397e09985167932f6817e070cc14cdd4ea3bbea
Alejandro Molina <jalemolina@gmail.com>
2013-12-08 02:26:21 -0300

    solucionada la instalación de inconsolata para powerline

--- Inconsolata-Powerline/Inconsolata-Powerline.sh
+++ Inconsolata-Powerline/Inconsolata-Powerline.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Ejecutar como admin.
 
+export LC_COLLATE=es_AR.utf8
+export LANG=es_AR.utf8
+export LANGUAGE=es_AR.utf8
 
 cp -r etc/ /etc/
 cp -r usr/ /usr/
@@ -8,6 +11,9 @@ cp -r usr/ /usr/
 cp 10-powerline-symbols.conf ~/.fonts.conf.d/
 cp 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
 cp PowerlineSymbols.otf ~/.fonts/
+cp ~/.vim/bundle/vim-powerline/fontpatcher/*.ttf ~/.fonts/
+cp ~/.vim/bundle/vim-powerline/fontpatcher/*.ttf /usr/share/fonts/
 
+fc-cache -vf
 fc-cache -vf ~/.fonts
 fc-cache -vf /usr/share/fonts
