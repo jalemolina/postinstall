1021955f15cd7673b3f3ad06f3823f2fe9cb9a33
Alejandro Molina <jalemolina@gmail.com>
2014-04-10 19:44:17 -0300

    Packete hyphenation para latex, separación de sílabas

--- postinstall.sh
+++ postinstall.sh
@@ -78,7 +78,7 @@ yum -y install audacious audacious-libs audacious-plugins audacious-plugins-free
 yum -y install openclipart
 
 # LaTeX
-yum -y install texlive-svg texlive-svg-inkscape-doc texlive-pdftex pstoedit pdf2svg texlive-pdftools texlive-inconsolata texlive-inconsolata-doc writer2latex texlive-tex-gyre* texlive-collection-fontsrecommended texlive-pst* texlive-collection-pstricks texlive-svgcolor texlive-spot texlive-biolinum* texlive-libertine* texlive-libgreek texlive-cantarell texlive-psnfss
+yum -y install texlive-svg texlive-svg-inkscape-doc texlive-pdftex pstoedit pdf2svg texlive-pdftools texlive-inconsolata texlive-inconsolata-doc writer2latex texlive-tex-gyre* texlive-collection-fontsrecommended texlive-pst* texlive-collection-pstricks texlive-svgcolor texlive-spot texlive-biolinum* texlive-libertine* texlive-libgreek texlive-cantarell texlive-psnfss texlive-babel texlive-babel-spanish texlive-hyphen-spanish
 
 # Inkscape
 yum -y install inkscape inkscape-docs inkscape-sozi inkscape-view
